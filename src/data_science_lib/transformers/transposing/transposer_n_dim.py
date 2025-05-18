577a2837690cee16edf769b528039b442ce11fa9
Leon David Zipp <leondavidzipp@gmx.de>
2025-05-18 13:27:41 +0200

    typing fixes

--- src/data_science_lib/transformers/transposing/transposer_n_dim.py
+++ src/data_science_lib/transformers/transposing/transposer_n_dim.py
@@ -3,11 +3,19 @@ import sklearn as sk
 
 
 class TransposerNDim(sk.base.BaseEstimator, sk.base.TransformerMixin):
-	def __init__(self, transpose_dims: tuple):
+	def __init__(self, transpose_dims: tuple[int, ...]):
 		self.transpose_dims = transpose_dims
 
-	def fit(self, X: np.ndarray, y=None):
+	def fit(self, X: np.ndarray, y=None) -> "TransposerNDim":  # type: ignore
+		"""
+		Only provided for sklearn compatibility.
+		Args:
+			X (np.ndarray): Does nothing, but is required for sklearn compatibility.
+			y: Does nothing, but is required for sklearn compatibility.
+		Returns:
+			self: Fitted transposer.
+		"""
 		return self
 
-	def transform(self, X: np.ndarray, y=None):
-		return np.transpose(X, self.transpose_dims)
+	def transform(self, X: np.ndarray, y=None) -> np.ndarray:  # type: ignore
+		return np.transpose(X, self.transpose_dims)  # type: ignore
--- src/data_science_lib/transformers/transposing/transposer_n_dim_test.py
+++ src/data_science_lib/transformers/transposing/transposer_n_dim_test.py
@@ -1,37 +1,37 @@
 import unittest
 import numpy as np
-from src.data_science_lib.transformers.transposing.TransposerNDim import TransposerNDim
+from .transposer_n_dim import TransposerNDim
 
 
 class TestTransposerNDim(unittest.TestCase):
 	def test_transpose_2d_array(self):
 		X = np.array([[1, 2, 3], [4, 5, 6]])
 		transposer = TransposerNDim(transpose_dims=(1, 0))
-		transposer.fit(X)
-		transformed = transposer.transform(X)
+		transposer.fit(X) # type: ignore
+		transformed = transposer.transform(X) # type: ignore
 		expected = np.array([[1, 4], [2, 5], [3, 6]])
-		np.testing.assert_array_equal(transformed, expected)
+		np.testing.assert_array_equal(transformed, expected) # type: ignore
 
 	def test_transpose_3d_array(self):
 		X = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
 		transposer = TransposerNDim(transpose_dims=(2, 0, 1))
-		transposer.fit(X)
-		transformed = transposer.transform(X)
+		transposer.fit(X) # type: ignore
+		transformed = transposer.transform(X) # type: ignore
 		expected = np.array([[[1, 3], [5, 7]], [[2, 4], [6, 8]]])
-		np.testing.assert_array_equal(transformed, expected)
+		np.testing.assert_array_equal(transformed, expected) # type: ignore
 
 	def test_invalid_transpose_dims(self):
 		X = np.array([[1, 2, 3], [4, 5, 6]])
 		transposer = TransposerNDim(transpose_dims=(0, 2))  # Invalid dimensions
 		with self.assertRaises(ValueError):
-			transposer.transform(X)
+			transposer.transform(X) # type: ignore
 
 	def test_no_transpose(self):
 		X = np.array([[1, 2, 3], [4, 5, 6]])
 		transposer = TransposerNDim(transpose_dims=(0, 1))  # No change in dimensions
-		transposer.fit(X)
-		transformed = transposer.transform(X)
-		np.testing.assert_array_equal(transformed, X)
+		transposer.fit(X) # type: ignore
+		transformed = transposer.transform(X) # type: ignore
+		np.testing.assert_array_equal(transformed, X) # type: ignore
 
 
 if __name__ == "__main__":
